20724ae0cc09ec115d4bfd90020472fc987aa80d
fuzzy-yusuke <youth_k0228@outlook.com>
2020-11-25 00:02:05 +0900

    header component install

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return view('app');
+}) -> where ('any','.*'); //初回アクセス時はLaravel側でリクエストを受け取る
