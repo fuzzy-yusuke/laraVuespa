315473e5f0ceaab6a5447b60f3702388a6c7adb4
fuzzy-yusuke <youth_k0228@outlook.com>
2020-12-09 23:58:41 +0900

    詳細取得API繋ぎ込み完了

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -3,26 +3,26 @@
         <div class="row justify-content-center">
             <div class="col-sm-6">
                 <form>
+                    <!--以下、入力して受け取ったそれぞれのデータを「v-model」でVueインスタンスに連動させて表示-->
                     <div class="form-group row border-bottom">
                         <label for="id" class="col-sm-3 col-form-label">ID</label>
-                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-bind:value="taskId">
-                        <!--受け取ったtaskIdを「v-bind:value="taskId"」で動的に表示-->
+                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-model="taskId">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="title" class="col-sm-3 col-form-label">Title</label>
-                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="title" value="title title">
+                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="title" v-model="task.title">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="content" class="col-sm-3 col-form-label">content</label>
-                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="content" value="content content">
+                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="content" v-model="task.content">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
-                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="person-in-charge" value="Ichiro">
+                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="person-in-charge" v-model="task.person_in_charge">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="task-date" class="col-sm-3 col-form-label">Task Date</label>
-                        <input type="date" class="col-sm-9 form-control-plaintext" readonly id="task-date" value="2020-12-14">
+                        <input type="date" class="col-sm-9 form-control-plaintext" readonly id="task-date" v-model="task.task_date">
                     </div>
                 </form>
             </div>
@@ -32,8 +32,24 @@
 
 <script>
 export default {
-    props: {
+    props:{
         taskId: String //taskIdをURLパラメータとして受け取る
+    },
+    data:{
+        return{
+            task:{}
+        }
+    },
+    methods:{
+        getTask(){      //getTask()でAPIからタスクデータを取得
+            axios.get('/api/tasks/'+this.taskId)
+                 .then((res) => {
+                     this.task=res.data;
+                 });
+        }
+    },
+    mounted(){
+        this.getTask(); //画面が表示される時にメソッドが呼び出される
     }
 }
 </script>
